3a07a246f951b9c7925f2aa51bc5e1850ef0da3f
Dhaval Kapil <dhavalkapil@gmail.com>
2014-08-02 14:50:54 +0530

    Added links in header

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
+        anchors: ['home_screen', 'events_screen', 'about_screen', 'team_screen', 'calendar_screen', 'contact_screen'],
     	slidesNavigation: true,
     	resize: true,
     	scrollOverflow: true,
     	menu: 'nav',
     	css3: true,
-        paddingTop: '100px'
+        paddingTop: '100px',
+        menu: '.nav'
     });
     /*$('.section').slimScroll({
     	width: '100%'
